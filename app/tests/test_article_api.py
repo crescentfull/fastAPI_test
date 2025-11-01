399508e6b26680fe900c6ec4e4420725f81f1e67
Yeongrok Song <song7868@gmail.com>
2025-11-01 22:55:49 +0900

    feat: test_create_article_returns_json_fields

--- app/tests/test_article_api.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from httpx import AsyncClient
-from main import app
-
-async def test_create_article_returns_201():
-    async with AsyncClient(app=app, base_url="http://test") as client:
